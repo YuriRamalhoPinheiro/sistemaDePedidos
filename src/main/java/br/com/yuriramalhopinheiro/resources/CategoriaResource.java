7225d2cdd25b5041ecb705d3f6b10844eb787189
Yuri Ramalho Pinheiro <yuri_uy@hotmail.com>
2018-05-03 23:50:01 -0300

    Adicionando primeira classe de domínio - Categoria

--- src/main/java/br/com/yuriramalhopinheiro/resources/CategoriaResource.java
+++ src/main/java/br/com/yuriramalhopinheiro/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package br.com.yuriramalhopinheiro.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.yuriramalhopinheiro.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+	
+		Categoria categoria1 = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Eletrônico");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(categoria1);
+		lista.add(categoria2);
+		
+		return lista;
 	}
 	
 }
