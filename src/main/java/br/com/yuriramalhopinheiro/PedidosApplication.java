ea626bb5d9b26619819afd7d65c348e21a14cf64
Yuri Ramalho Pinheiro <yuri_uy@hotmail.com>
2018-05-05 16:34:41 -0300

    Associação muitos para muitos entre Produto e Categoria

--- src/main/java/br/com/yuriramalhopinheiro/PedidosApplication.java
+++ src/main/java/br/com/yuriramalhopinheiro/PedidosApplication.java
@@ -8,13 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.yuriramalhopinheiro.domain.Categoria;
+import br.com.yuriramalhopinheiro.domain.Produto;
 import br.com.yuriramalhopinheiro.repositories.CategoriaRepository;
+import br.com.yuriramalhopinheiro.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class PedidosApplication implements CommandLineRunner{
 	
 	@Autowired
-	private CategoriaRepository repository; 
+	private CategoriaRepository categoriaRepository; 
+	
+	@Autowired
+	private ProdutoRepository produtoRepository; 
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApplication.class, args);
@@ -26,6 +32,18 @@ public class PedidosApplication implements CommandLineRunner{
 		Categoria categoria1 = new Categoria(null, "Informática");
 		Categoria categoria2 = new Categoria(null, "Eletrônicos");
 		
-		this.repository.saveAll(Arrays.asList(categoria1, categoria2));
+		Produto produto1 = new Produto(null, "Computador", 2000.00);
+		Produto produto2 = new Produto(null, "Impressora", 800.00);
+		Produto produto3 = new Produto(null, "Mause", 80.00);
+		
+		categoria1.setProdutos(Arrays.asList(produto1, produto2, produto3));
+		categoria2.setProdutos(Arrays.asList(produto1));
+		
+		produto1.setCategorias(Arrays.asList(categoria1, categoria2));
+		produto2.setCategorias(Arrays.asList(categoria1, categoria2));
+		produto3.setCategorias(Arrays.asList(categoria1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		this.produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
 	}
 }
