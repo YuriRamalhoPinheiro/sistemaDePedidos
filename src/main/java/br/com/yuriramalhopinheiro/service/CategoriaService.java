fda4aa3c668f3e76377c98a42d9bac5553e4d5b7
Yuri Ramalho Pinheiro <yuri_uy@hotmail.com>
2018-05-05 15:13:31 -0300

    Adicionando o Service e Repository para Categoria

---
+++ src/main/java/br/com/yuriramalhopinheiro/service/CategoriaService.java
@@ -0,0 +1,24 @@
+package br.com.yuriramalhopinheiro.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.yuriramalhopinheiro.domain.Categoria;
+import br.com.yuriramalhopinheiro.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria buscarPorId(Integer id) {
+		
+		Optional<Categoria> object = repository.findById(id);
+		
+		return object.orElse(null);
+	}
+	
+}
